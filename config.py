810192ddee933b263b8eb7b1dbe22413a53b733c
[chrpkorir] <faythkoko@gmail.com>
2022-11-09 10:03:19 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.ENVIRON.GET('SECRET_KEY', 'secret')
+    STREAM_API_KEY = ''  # Insert your Stream API key her
+    STREAM_SECRET = ''  # Insert your Stream Secret here
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
+
+
+class TestingConfig(Config):
+    Testing = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+                              'sqlite://'
+    WTF_CSRF_ENABLED = False
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'data/sqite')
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
