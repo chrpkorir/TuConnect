7d62efc1ccbb547aed011afb8e1c232cbbebdc2d
[chrpkorir] <faythkoko@gmail.com>
2022-11-09 11:56:30 +0300

    Added Configurations

--- config.py
+++ config.py
@@ -5,9 +5,20 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.ENVIRON.GET('SECRET_KEY', 'secret')
-    STREAM_API_KEY = ''  # Insert your Stream API key her
+    STREAM_API_KEY = ''  # Insert your Stream API key here
     STREAM_SECRET = ''  # Insert your Stream Secret here
-
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+        ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    OFFBRAND_MAIL_SUBJECT_PREFIX = '[Offbrand]'
+    OFFBRAND_MAIL_SENDER = 'Offbrand Admin <admin@offbrand.co>'
+    OFFBRAND_ADMIN = os.environ.get('OFFBRAND_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
     @staticmethod
     def init_app(app):
         pass
