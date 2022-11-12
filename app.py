3bfb9384f9a80d147e8eeb6bfbfbbea705520949
[chrpkorir] <faythkoko@gmail.com>
2022-11-12 16:20:32 +0300

    Migrations

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 import os
+from flask_migrate import Migrate, upgrade
 from app import create_app, db
+from app.models import User, Role, Permission
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role,
+                Permission=Permission)
+
+
+@app.cli.command()
+def deploy():
+    """Running all of our deployment operations."""
+    # migrate database to latest revision
+    upgrade()
+
+    # create or update user roles
+    Role.insert_roles()
